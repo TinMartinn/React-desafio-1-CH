e7a05829e71376e1337ef15a879561748b55c816
Martín Arzamendia <martin.arzamendia@gmail.com>
2022-09-12 23:33:03 -0300

    desafio 4

--- src/components/text.js
+++ src/components/text.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { useState } from 'react';
+
+
 
 const Text = () => {
+    const [cantidad, setcantidad] = useState(0);
+    const subir = () => {
+        if (cantidad < 5) {
+            setcantidad(cantidad + 1)
+        }
+    }
+    const bajar = () => {
+        if (cantidad > 0) {
+            setcantidad(cantidad - 1)
+        }
+    }
     return (
         <div>
             <h1>
                 Todos nuestros productos estaran disponibles muy pronto
             </h1>
+            <div  className='boxGeneral'>
+                <button className='boton' onClick={subir}>+</button>{cantidad}
+                <button className='boton' onClick={bajar}>-</button>
+            </div>
         </div>
     );
 }
